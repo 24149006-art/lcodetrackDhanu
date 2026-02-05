02f67c5fd859d151fa699921d1483b7121f39528
24149006-art <24149006@drngpit.ac.in>
2026-02-05 10:08:46 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,11 @@
+// Last updated: 2/5/2026, 10:08:45 AM
+class Solution {
+    public int hammingWeight(int n) {
+       int count=0;
+       while(n!=0){
+        n=(n&(n-1));
+        count++;   
+       }
+       return count; 
+    }
+}
