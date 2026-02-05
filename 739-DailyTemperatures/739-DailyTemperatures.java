10428b0b24aa80dcbb82f0fa510cca5d4aa24bfd
24149006-art <24149006@drngpit.ac.in>
2026-02-05 10:08:16 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -0,0 +1,16 @@
+// Last updated: 2/5/2026, 10:08:15 AM
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        int n = temp.length;
+        int[] res = new int[n];
+        Stack<Integer> s = new Stack();
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && temp[i] > temp[s.peek()]) {
+                int x = s.pop();
+                res[x] = i - x;
+            }
+            s.push(i);
+        }
+        return res;
+    }
+}
