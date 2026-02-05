592fb72ee8e599de25e005c1d6da0e91a7cb358a
24149006-art <24149006@drngpit.ac.in>
2026-02-05 10:09:04 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,22 @@
+// Last updated: 2/5/2026, 10:09:04 AM
+class Solution {
+    public boolean isValid(String str) {
+        Stack<Character>s=new Stack();
+        char[] ch=str.toCharArray();
+        for(char c:ch){
+            if(c=='('|| c=='['|| c=='{')
+            s.push(c);
+            else{
+                if(s.empty()) return false;
+                else if(c==')' && s.peek()=='(')
+                s.pop();
+                else if(c==']' && s.peek()=='[')
+                s.pop();
+                else if(c=='}' && s.peek()=='{')
+                s.pop();
+                else return false;
+            }
+        }
+        return s.empty();         
+    }
+}
