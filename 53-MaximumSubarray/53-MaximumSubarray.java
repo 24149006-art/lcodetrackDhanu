33ef09ff49e943f58d03ce87634e7ff43e71366c
24149006-art <24149006@drngpit.ac.in>
2026-02-05 10:09:02 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,16 @@
+// Last updated: 2/5/2026, 10:09:02 AM
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ms=nums[0];
+        int cs=0;
+        for(int i=0;i<nums.length;i++){
+            cs+=nums[i];
+            if(cs>ms)
+                ms=cs;
+            if(cs<0)
+                cs=0;
+        }
+        return ms;
+    }
+}
+                
