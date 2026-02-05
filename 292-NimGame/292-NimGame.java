47b1c42fdaa962c8b28de2b3934002b1cb1b9152
24149006-art <24149006@drngpit.ac.in>
2026-02-05 10:08:35 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,8 @@
+// Last updated: 2/5/2026, 10:08:35 AM
+class Solution {
+    public boolean canWinNim(int n) {
+       // if(n%4==0) return false;
+       // else return true;
+       return n%4!=0 ? true:false;
+    }
+}
