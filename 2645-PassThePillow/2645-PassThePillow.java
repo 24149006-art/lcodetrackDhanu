672e1ad7760a700dcf61e69faa247156fd880cf7
24149006-art <24149006@drngpit.ac.in>
2026-02-05 10:07:24 +0530

    Create 2645-PassThePillow

---
+++ 2645-PassThePillow/2645-PassThePillow.java
@@ -0,0 +1,13 @@
+// Last updated: 2/5/2026, 10:07:24 AM
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cycle=time/(n-1);
+        int rem=time%(n-1);
+        if(cycle%2==1){
+            return n-rem;
+        }
+        else{
+            return rem+1;
+        }
+    }
+}
